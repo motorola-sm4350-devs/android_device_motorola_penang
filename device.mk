7d0d864e8a746beee4d6447f2caa172dde39aa8e
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-11-10 22:27:37 +0900

    penang: Build supplicant AIDL interfaces to vendor Change-Id: I0069e31d39422af7cf23eb6a587d4d8254efbe58 Signed-off-by: CHRISL7 <70589286+CHRISL7@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -601,6 +601,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.wifi.supplicant-V1-ndk.vendor \
+    vendor.qti.hardware.wifi.supplicant-V1-ndk.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
